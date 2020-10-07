c57e5c1a9f02754f27f6d606e56669519db625ed
Aniket Pingley <aniket.pingley@gmail.com>
2020-10-07 20:59:57 +0530

    copy constructor and assignment operator

--- day13/code.cpp
+++ day13/code.cpp
@@ -10,29 +10,46 @@ int func(double f); // free function
 
 class Sample{
     private:
-        int h; // member variables
-        double n; // member variables
+        int _h; // member variables
+        double _n; // member variables
         int func2(char){return 0;} // method
         int func3(char){return 0;} // method
     
     public:
         double p; // is this global? NO.
-        Sample(){
-            h = 9;
-            n = 4.5;
-            std::cout << __func__ << std::endl;
+        Sample(){ // default constructor
+            _h = 9;
+            _n = 4.5;
+            std::cout << "default constructor " << __func__ << std::endl;
+        }
+
+        Sample(const int& k, const double& l){ 
+            // overloaded constuctor 
+            _h = k;
+            _n = l;
+            std::cout << "overloaded constructor " << __func__ << std::endl;
+        }
+
+        Sample(const Sample& s){
+            _h = s._h;
+            _n = s._n;
+            std::cout << "copy constructor " << __func__ << std::endl;
+        }
+
+        void operator = (const Sample& d){
+            std::cout << "assignment operator " << __func__ << std::endl;
         }
 
         int getValueH(){ // getter function
-            return h;
+            return _h;
         }
 
         double getValueN(){
-            return n;
+            return _n;
         }
 
         void setValueH(int i){ // setter function
-            h = i;
+            _h = i;
         }
 
         ~Sample(){
@@ -46,7 +63,7 @@ int main(){
     std::cout << s.getValueH() << "\t" << s.getValueN() << std::endl;
     s.setValueH(67);
     std::cout << s.getValueH() << "\t" << s.getValueN() << std::endl;
-    //Sample r; // stack
+    Sample r; // stack
     
     Sample *t = new Sample(); // heap
     std::cout << t->getValueH() << "\t" << t->getValueN() << std::endl;
@@ -61,4 +78,17 @@ int main(){
     t->p;
 
     delete t; // destroy to object
+
+    Sample overloadedSample = Sample(90, 6.5); 
+    std::cout << overloadedSample.getValueH() \
+    << "\t" << overloadedSample.getValueN() << std::endl;
+
+    Sample d; // default constructor
+    Sample s2 = overloadedSample; // copy constructor
+    std::cout << s2.getValueH() \
+    << "\t" << s2.getValueN() << std::endl;
+    Sample& s3 = overloadedSample; // no constructor
+    d = overloadedSample; // assignment operator
+
+    Sample *ptrS2 = &overloadedSample; // no constructor
 }
